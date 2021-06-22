7a7c30229a5d15f61c9317d02cae267b658a5426
AKIN GAN <akin.j.a.gan@oocl.com>
2021-06-22 22:06:02 +0800

    replace get lineItem general information for OrderReceipt

--- src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
@@ -11,7 +11,6 @@ public class OrderReceipt {
     private static final String PRINTING_ORDER_RECEIPT_HEADER = "======Printing Orders======";
     private static final String SALES_TAX = "Sales Tax";
     private static final String TOTAL_AMOUNT = "Total Amount";
-    public static final double TEN_PERCENTAGE_SALES_TAX = 0.10d;
     public static final char WORD_SEPARATOR = '\t';
     public static final char LINE_SEPARATOR = '\n';
     private Order order;
@@ -36,16 +35,7 @@ public class OrderReceipt {
         double totSalesTx = 0d;
         double tot = 0d;
         for (LineItem lineItem : order.getLineItems()) {
-            output.append(lineItem.getDescription());
-            output.append(WORD_SEPARATOR);
-            output.append(lineItem.getPrice());
-            output.append(WORD_SEPARATOR);
-            output.append(lineItem.getQuantity());
-            output.append(WORD_SEPARATOR);
-            output.append(lineItem.totalAmount());
-            output.append(LINE_SEPARATOR);
-
-            // calculate sales tax @ rate of 10%
+            output.append(lineItem.getGeneralInformation());
             double salesTax = lineItem.subSaleTax();
             totSalesTx += salesTax;
 
