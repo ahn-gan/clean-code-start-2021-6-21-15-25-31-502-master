49115862f32b81d67c08b92a9256ecd1a85ba092
AKIN GAN <akin.j.a.gan@oocl.com>
2021-06-22 15:06:13 +0800

    inline Order's property

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -5,23 +5,16 @@ public class Order {
     private final User user;
     private int orderNumber;
 
-    private String buyerName;
-    private String buyerPhoneNumber;
-    private String buyerAddress;
-
 
     public Order(int orderNumber, User user) {
         this.orderNumber = orderNumber;
 
         this.user = user;
-        this.buyerName = this.user.getBuyerName();
-        this.buyerPhoneNumber = this.user.getBuyerPhoneNumber();
-        this.buyerAddress = this.user.getBuyerAddress();
     }
 
     public String confirm(){
         return String.format("Please confirm buyer information: buyer name is %s, " +
-                "buyer phone number is %s and buyer address is %s.", buyerName, buyerPhoneNumber, buyerAddress);
+                "buyer phone number is %s and buyer address is %s.", this.user.getBuyerName(), this.user.getBuyerPhoneNumber(), this.user.getBuyerAddress());
     }
 }
 
